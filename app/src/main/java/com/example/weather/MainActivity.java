36e7f039d16e59fbe7c1a9b6c75bed6ccfbe3b8d
Hayman <habdelj@uwo.ca>
2020-04-28 14:27:52 -0400

    Connected to weather api

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -9,7 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -18,94 +27,55 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView cityName;
-    Button searchbtn;
-    TextView result;
-
-
-    //First String mmeans URL is in String , Void means nothing and the last string means return type will be string
-    class Weather extends AsyncTask<String,Void,String>{
-
-            @Override
-        protected String doInBackground(String... address) {
-            //String.. means multiple address can be sent. it acts as an array
-            try {
-                URL url = new URL(address[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                //Establish connection with address
-                connection.connect();
-
-                //retrieve data from url
-                InputStream is = connection.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-
-                //Retrieve data and return it as String
-                int data = isr.read();
-                String content = "";
-                char ch;
-                while (data > 0){
-                    ch = (char) data;
-                    content = content + ch;
-                    data = isr.read();
-                }
-                Log.i("Content",content);
-                return content;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-    }
-
-    public void search(View view) {
-        cityName = findViewById(R.id.cityName);
-        searchbtn = findViewById(R.id.searchbtn);
-        result = findViewById(R.id.resultbtn);
-
-        String cName = cityName.getText().toString();
-
-        String content;
-        Weather weather = new Weather();
-        try {
-            content = weather.execute("https://openweathermap.org/data/2.5/weather?q="+ cName +"&appid=439d4b804bc8187953eb36d2a8c26a02").get();
-            //First we will check data is retrieve successfully or not
-            Log.i("contentData",content);
-
-            //JSON
-            JSONObject jsonObject = new JSONObject(content);
-            String weatherData = jsonObject.getString("weather");
-            Log.i("weatherData",weatherData);
-
-            //weather data in array
-            JSONArray array = new JSONArray(weatherData);
-
-            String main = "",description = "";
-
-            for(int i=0; i<array.length(); i++){
-                JSONObject weatherPart = array.getJSONObject(i);
-                main = weatherPart.getString("main");
-                description = weatherPart.getString("description");
-            }
-            Log.i("main",main);
-            Log.i("description",description);
-
-            result.setText("Main :" +main+ "\nDescription : " + description);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
 
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.weatherapi.com/v1/current.json?key=e39229688fd34bada27231316202704&q=l4g3c7";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            response = response.getJSONObject("location");
+                                String name = response.getString("name");
+                                String country = response.getString("country");
+                                String localtime = response.getString("localtime");
+
+                                mTextViewResult.append(name + "," + country + "," + localtime + "\n\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
