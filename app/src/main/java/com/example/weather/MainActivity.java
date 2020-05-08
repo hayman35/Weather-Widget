305f69e4ab83b7f2e0441a28a133360a5f0c7a4f
Hayman <habdelj@uwo.ca>
2020-05-08 00:11:30 -0400

    Updated the api calls

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,86 +1,27 @@
 package com.example.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.weather.Weatherapi.Converter;
+import com.example.weather.Weatherapi.Temperatures;
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-public class MainActivity extends AppCompatActivity {
-    Editable Postcode;
-    private TextView mTextViewResult;
-    private TextView mtemp;
-    private EditText mtext;
-    private RequestQueue mQueue;
-    private ImageView icon;
-    private Button buttonParse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mTextViewResult = findViewById(R.id.textid1);
-        buttonParse = findViewById(R.id.buttonid);
-        mtext = findViewById(R.id.inputtext);
-        icon = findViewById(R.id.pictureid);
-        mtemp = findViewById(R.id.tempid);
-        mQueue = Volley.newRequestQueue(this);
-        buttonParse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Postcode = mtext.getText();
-                jsonParse();
-            }
-        });
     }
 
-    private void jsonParse() {
-        String url = "https://api.weatherapi.com/v1/forecast.json?key=e39229688fd34bada27231316202704&q="+Postcode+"&days=3\n";
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONObject location,current;
-                            location = response.getJSONObject("location");
-                            String name = location.getString("name");
-                            String country = location.getString("country");
-                            String localtime = location.getString("localtime");
-                            mTextViewResult.append(name + ", " + country + " ," + localtime + "\n\n");
-
-                            // The current temperature with the icon
-                            current = response.getJSONObject("current");
-                            String temp = current.getString("temp_c");
-                            JSONObject object = current.getJSONObject("condition");
-                            String iconurl = object.getString("icon");
-                            Picasso.get().load("https:"+iconurl).into(icon);
-                            mtemp.append(temp+ ", " +"\n\n");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
 }
--- app/src/main/java/com/example/weather/Weatherwidget.java
+++ app/src/main/java/com/example/weather/Weatherwidget.java
@@ -3,47 +3,35 @@ package com.example.weather;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.graphics.drawable.Icon;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.squareup.picasso.Picasso;
+import com.example.weather.Weatherapi.Temperatures;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class Weatherwidget extends AppWidgetProvider {
-    private TextView city_name, temp_info;
-    private ImageView img;
-    Weather weather;
-
 
     public void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        //CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weatherwidget);
-       // views.setTextViewText(R.id.appwidget_text, widgetText);
-
-         // views.setImageViewIcon(R.id.imageView5, Icon.createWithContentUri("D:\\Android Projects\\Weather\\app\\src\\images\\day\\113.png"));
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -52,15 +40,18 @@ public class Weatherwidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-        weather
-        city_name.findViewById(R.id.city_name);
-        temp_info.findViewById(R.id.temp_info);
+        API api = new API();
+        api.doInBackground();
+        Temperatures data = api.getData();
+        System.out.println("HEY" + data.getCurrent().getFeelslikeC());
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+
+
 }
 
